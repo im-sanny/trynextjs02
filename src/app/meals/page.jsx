ae00d5f773e1d5ce5d2f6315ec24d0d2811e12b4
im-sanny <raselparvezsanny@gmail.com>
2024-06-18 18:25:12 +0600

    added raw css in meals page

--- src/app/meals/page.jsx
+++ src/app/meals/page.jsx
@@ -1,14 +1,15 @@
 import Meals from '@/components/Meals';
 import React from 'react';
+import styles from './styles.module.css'
 
-const page = () => {
+const MilPage = () => {
     return (
         <div className='p-12 text-center'>
             <h2 className='text-3xl font-semibold text-teal-600'>Choose your meals</h2>
-            <p>Choose meals of your choice by searching.....</p>
+            <p className={styles.text_large}>Choose meals of your choice by searching.....</p>
             <Meals></Meals>
         </div>
     );
 };
 
-export default page;
+export default MilPage;
