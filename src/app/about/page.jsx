6a3d2eb5db76f3086066b4d06c2e7457b6935fb8
im-sanny <raselparvezsanny@gmail.com>
2024-06-23 22:32:45 +0600

    added time and used caching and revalidatiing

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,10 +9,18 @@ export const metadata = {
   keywords: ["about", "about page"],
 };
 
-const Page = () => {
+const getTime = async () => {
+  const res = await fetch("http://localhost:3000/time",{next: {revalidate: 5}});
+  const data = await res.json();
+  return data.currentTime;
+};
+
+const Page = async () => {
+  const currentTime = await getTime();
   return (
     <div className={`${headLand.className} min-h-screen px-12 py-24`}>
-      <h1>About Page</h1>
+      <h1 className="text-3xl">About Page</h1>
+      <h3 className="text-3xl text-red-400 mt-12">Time: {currentTime} </h3>
     </div>
   );
 };
