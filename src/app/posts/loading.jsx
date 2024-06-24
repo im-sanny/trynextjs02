4b8c6f14beb4409d48c7f580f876cb5ac6f476d3
im-sanny <raselparvezsanny@gmail.com>
2024-06-24 10:26:05 +0600

    added loader to show loading while data fetching

---
+++ src/app/posts/loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <div>
+            <h6 className="">Post data is Loading.....</h6>
+        </div>
+    );
+};
+
+export default Loading;
